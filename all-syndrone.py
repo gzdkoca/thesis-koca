06d609b557290dc70d5ac340d938795abb20020c
Gözdenur Koca <73363774+gzdkoca@users.noreply.github.com>
2024-06-10 10:48:37 +0200

    Update all-syndrone.py

--- all-syndrone.py
+++ all-syndrone.py
@@ -221,7 +221,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # Training loop
-num_epochs = 10
+num_epochs = 1
 start_time = time.time()
 
 train_losses = []
