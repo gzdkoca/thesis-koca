e6dcd0d8eec7472302b882cef618cf4388aea0c0
Gözdenur Koca <73363774+gzdkoca@users.noreply.github.com>
2024-12-04 16:12:05 +0100

    Update syndrone-acdc.py

--- syndrone-acdc.py
+++ syndrone-acdc.py
@@ -265,6 +265,9 @@ for epoch in range(num_epochs):
     
 print('Finished Training')
 
+torch.save(model.state_dict(), 'model_syndrone_acdc.pth')
+torch.save(model, 'model_syndrone_acdc_full.pth')
+
 # Print final accuracies
 print(f"Final Training Accuracy: {train_accuracies[-1]}%")
 print(f"Final Validation Accuracy: {val_accuracies[-1]}%")
