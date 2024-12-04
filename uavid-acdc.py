13ac6c6080e2495206e614e0739628b96d09a04d
Gözdenur Koca <73363774+gzdkoca@users.noreply.github.com>
2024-12-04 16:13:55 +0100

    Update uavid-acdc.py

--- uavid-acdc.py
+++ uavid-acdc.py
@@ -276,6 +276,9 @@ for epoch in range(num_epochs):
     
 print('Finished Training')
 
+torch.save(model.state_dict(), 'model_uavid_acdc.pth')
+torch.save(model, 'model_uavid_acdc_full.pth')
+
 # Print final accuracies
 print(f"Final Training Accuracy: {train_accuracies[-1]}%")
 print(f"Final Validation Accuracy: {val_accuracies[-1]}%")
