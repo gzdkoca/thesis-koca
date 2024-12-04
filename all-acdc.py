494f4123de36e88d04890abfa4978855c442c570
Gözdenur Koca <73363774+gzdkoca@users.noreply.github.com>
2024-12-04 15:01:29 +0100

    Update all-acdc.py

--- all-acdc.py
+++ all-acdc.py
@@ -290,6 +290,10 @@ for epoch in range(num_epochs):
     
 print('Finished Training')
 
+# save the model
+torch.save(model.state_dict(), 'model_all_acdc.pth')
+torch.save(model, 'model_all_acdc_full.pth')
+
 # Print final accuracies
 print(f"Final Training Accuracy: {train_accuracies[-1]}%")
 print(f"Final Validation Accuracy: {val_accuracies[-1]}%")
