0c745671f32ad2b511de58db7c8d79755b8c86fe
Gözdenur Koca <73363774+gzdkoca@users.noreply.github.com>
2024-12-04 16:12:35 +0100

    Update syndrone-selma.py

--- syndrone-selma.py
+++ syndrone-selma.py
@@ -275,6 +275,9 @@ for epoch in range(num_epochs):
     
 print('Finished Training')
 
+torch.save(model.state_dict(), 'model_syndrone_selma.pth')
+torch.save(model, 'model_syndrone_selma_full.pth')
+
 # Print final accuracies
 print(f"Final Training Accuracy: {train_accuracies[-1]}%")
 print(f"Final Validation Accuracy: {val_accuracies[-1]}%")
