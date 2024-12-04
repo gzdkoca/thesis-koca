ef1f5335b98d1f3450c2028b15cf409dc5009198
Gözdenur Koca <73363774+gzdkoca@users.noreply.github.com>
2024-12-04 16:14:52 +0100

    Update uavid-syndrone.py

--- uavid-syndrone.py
+++ uavid-syndrone.py
@@ -280,6 +280,9 @@ for epoch in range(num_epochs):
     
 print('Finished Training')
 
+torch.save(model.state_dict(), 'model_uavid_syndrone.pth')
+torch.save(model, 'model_uavid_syndrone_full.pth')
+
 # Print final accuracies
 print(f"Final Training Accuracy: {train_accuracies[-1]}%")
 print(f"Final Validation Accuracy: {val_accuracies[-1]}%")
