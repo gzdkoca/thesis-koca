bc894ad593274e27cff64bec3246d14fc711e3d0
Gözdenur Koca <73363774+gzdkoca@users.noreply.github.com>
2024-12-04 16:10:51 +0100

    Update selma-selma.py

--- selma-selma.py
+++ selma-selma.py
@@ -275,6 +275,9 @@ for epoch in range(num_epochs):
     
 print('Finished Training')
 
+torch.save(model.state_dict(), 'model_selma_selma.pth')
+torch.save(model, 'model_selma_selma_full.pth')
+
 # Print final accuracies
 print(f"Final Training Accuracy: {train_accuracies[-1]}%")
 print(f"Final Validation Accuracy: {val_accuracies[-1]}%")
